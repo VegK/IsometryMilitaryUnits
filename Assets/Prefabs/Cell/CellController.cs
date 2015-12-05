e5b023d0c1d0877dddce6cc4133b989d80e2d6af
VegK <eal.prog@gmail.com>
2015-12-05 19:20:59 +1100

    Added cell prefab

---
+++ Assets/Prefabs/Cell/CellController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class CellController : MonoBehaviour
+{
+	#region Properties
+	#region Public
+	
+	#endregion
+	#region Private
+
+	#endregion
+	#endregion
+
+	#region Methods
+	#region Public
+	
+	#endregion
+	#region Private
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+	#endregion
+	#endregion
+}
